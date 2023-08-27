0b947b6e549fcf88bda6b9663124f54479e327ec
Noman <noman.khan3701@gmail.com>
2023-08-27 16:57:39 +0530

    Fixed faltu ke errors

--- src/app/coupon/page.jsx
+++ src/app/coupon/page.jsx
@@ -1,55 +1,8 @@
-"use client";
+import React from "react";
+import Coupons from "@/components/Coupons/Coupons";
 
-import React, { useEffect, useState } from 'react'
-import { IoMdPerson } from 'react-icons/io'
-import profilePic from '@/assets/images/profilePic.png'
-import { FiPlus } from 'react-icons/fi'
-import Image from 'next/image';
+const Page = () => {
+  return <Coupons />;
+};
 
-const coupon = () => {
-    const [scrolledDown, setScrolledDown] = useState(false)
-    const [lastScrollY, setLastScrollY] = useState(0)
-
-    useEffect(() => {
-        window.addEventListener("scroll", handleNavScroll)
-        return () => window.removeEventListener("scroll", handleNavScroll)
-    }, [lastScrollY])
-
-    const handleNavScroll = () => {
-        if (typeof window !== "undefined") {
-            const currentScrollPos = window.pageYOffset
-            if (currentScrollPos > lastScrollY && currentScrollPos > 80) {
-                // if scroll down hide the navbar
-                setScrolledDown(true)
-            } else {
-                // if scroll up show the navbar
-                setScrolledDown(false)
-            }
-            setLastScrollY(currentScrollPos)
-        }
-    }
-
-    return (
-        <div>
-            <div className={`Navbar ${scrolledDown ? 'scrolled' : ''}`}>
-                <div className="nav_left">
-                    <div className="btn_primary">
-                        <FiPlus />
-                        Add New Company
-                    </div>
-                </div>
-                <div className="nav_right">
-                    <div className="search">
-                        <input type="text" placeholder='Search Here...' />
-                    </div>
-                    <div className="profile">
-                        <Image height={20} width={40} src={profilePic} alt="profilePic" />
-                    </div>
-                </div>
-            </div>
-        </div>
-
-    )
-}
-
-export default coupon
+export default Page;
